b7b84117a1481a16a23a58daf43a69fa50be6744
sigwo <signalwo@gmail.com>
2014-07-07 17:40:44 -0400

    update OS X GUI build

--- src/net.cpp
+++ src/net.cpp
@@ -57,7 +57,8 @@ static bool vfLimited[NET_MAX] = {};
 static CNode* pnodeLocalHost = NULL;
 CAddress addrSeenByPeer(CService("0.0.0.0", 0), nLocalServices);
 uint64_t nLocalHostNonce = 0;
-array<int, THREAD_MAX> vnThreadsRunning;
+boost::array<int, THREAD_MAX> vnThreadsRunning;
+boost::detail::atomic_count vaMultiThreads1(0);
 static std::vector<SOCKET> vhListenSocket;
 CAddrMan addrman;
 
